c7e462410a054099cd52828a017113d8dec4ffd2
Robert Castley <robert.castley@gmail.com>
2021-10-22 12:58:03 +0100

    Added argparse, code formatting

--- iracing_send.py
+++ iracing_send.py
@@ -6,144 +6,146 @@ import os
 import time
 import sys
 from datetime import datetime
-#Import SignalFx Client Library - you may have to install these two libraries via "pip install signalfx" & "pip install irsdk"
 import signalfx
 import irsdk
+import argparse
 
-################################
-###Insert Your Variables Here###
-################################
 
-racer_name = 'YOUR_NAME_HERE'
-ingest_token = 'YOUR_TOKEN_HERE'
+# Syntax:
+# python3 iracing_send.py -name <YOUR_NAME_HERE> -token <YOUR_TOKEN_HERE>
+#
+parser = argparse.ArgumentParser(description="DataDrivers - iRacing Metric Sender")
+parser.add_argument("-n", "--name", help="Your iRacing Racer Name", required=True)
+parser.add_argument("-t", "--token", help="Splunk Ingest Token", required=True)
+args = vars(parser.parse_args())
 
 #################################
-#Don't touch anything below this#
+# Don't touch anything below this#
 #################################
 
-#SIM variables
-client = signalfx.SignalFx(ingest_endpoint='https://ingest.us1.signalfx.com')
-ingest = client.ingest(ingest_token)
+# SIM variables
+client = signalfx.SignalFx(ingest_endpoint="https://ingest.us1.signalfx.com")
+ingest = client.ingest(args["token"])
 
-#Uncomment the following line to enable debug logging
-#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# Uncomment the following line to enable debug logging
+# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 # create dict for values of txt from github pyirsdk repo
-#dictionary for metrics data
+# dictionary for metrics data
 
 metrics_dict = {
-    'AirDensity': '',
-    'AirPressure': '',
-    'AirTemp': '',
-    'BrakeRaw': '',
-    'EngineWarnings': '',
-    'FastRepairAvailable': '',
-    'FastRepairUsed': '',
-    'FogLevel': '',
-    'FrameRate': '',
-    'FuelLevel': '',
-    'FuelLevelPct': '',
-    'FuelUsePerHour': '',
-    'Gear': '',
-    'Lap': '',
-    'LapBestLap': '',
-    'LapBestLapTime': '',
-    'LapCompleted': '',
-    'LapCurrentLapTime': '',
-    'LapDeltaToBestLap_DD': '',
-    'LapDeltaToBestLap': '',
-    'LapDeltaToOptimalLap_DD': '',
-    'LapDeltaToOptimalLap': '',
-    'LapDeltaToSessionBestLap_DD': '',
-    'LapDeltaToSessionBestLap': '',
-    'LapDeltaToSessionLastlLap_DD': '',
-    'LapDeltaToSessionLastlLap': '',
-    'LapDeltaToSessionOptimalLap_DD': '',
-    'LapDeltaToSessionOptimalLap': '',
-    'LapDist': '',
-    'LapDistPct': '',
-    'LapLasNLapSeq': '',
-    'LapLastLapTime': '',
-    'LapLastNLapTime': '',
-    'LFtempCL': '',
-    'LFtempCM': '',
-    'LFtempCR': '',
-    'LFwearL': '',
-    'LFwearM': '',
-    'LFwearR': '',
-    'LRtempCL': '',
-    'LRtempCM': '',
-    'LRtempCR': '',
-    'LRwearL': '',
-    'LRwearM': '',
-    'LRwearR': '',
-    'OilLevel': '',
-    'OilPress': '',
-    'OilTemp': '',
-    'PlayerCarClassPosition': '',
-    'PlayerCarDriverIncidentCount': '',
-    'PlayerCarIdx': '',
-    'PlayerCarMyIncidentCount': '',
-    'PlayerCarPosition': '',
-    'PlayerCarTeamIncidentCount': '',
-    'PlayerCarTowTime': '',
-    'PlayerTireCompound': '',
-    'PlayerTrackSurface': '',
-    'PlayerTrackSurfaceMaterial': '',
-    'RaceLaps': '',
-    'RelativeHumidity': '',
-    'RFtempCL': '',
-    'RFtempCM': '',
-    'RFtempCR': '',
-    'RFwearL': '',
-    'RFwearM': '',
-    'RFwearR': '',
-    'Roll': '',
-    'RollRate': '',
-    'RPM': '',
-    'RRtempCL': '',
-    'RRtempCM': '',
-    'RRtempCR': '',
-    'RRwearL': '',
-    'RRwearM': '',
-    'RRwearR': '',
-    'SessionFlags': '',
-    'SessionLapsRemain': '',
-    'SessionNum': '',
-    'SessionState': '',
-    'SessionTime': '',
-    'SessionTimeRemain': '',
-    'SessionUniqueID': '',
-    'ShiftGrindRPM': '',
-    'ShiftIndicatorPct': '',
-    'ShiftPowerPct': '',
-    'Skies': '',
-    'Speed': '',
-    'SteeringWheelAngle': '',
-    'SteeringWheelAngleMax': '',
-    'SteeringWheelPctDamper': '',
-    'SteeringWheelPctTorque': '',
-    'SteeringWheelPctTorqueSign': '',
-    'SteeringWheelPctTorqueSignStops': '',
-    'SteeringWheelPeakForceNm': '',
-    'SteeringWheelTorque': '',
-    'ThrottleRaw': '',
-    'TireSetsAvailable': '',
-    'TireSetsUsed': '',
-    'TrackTemp': '',
-    'TrackTempCrew': '',
-    'WeatherType': '',
-    'WindDir': '',
-    'WindVel': ''
+    "AirDensity": "",
+    "AirPressure": "",
+    "AirTemp": "",
+    "BrakeRaw": "",
+    "EngineWarnings": "",
+    "FastRepairAvailable": "",
+    "FastRepairUsed": "",
+    "FogLevel": "",
+    "FrameRate": "",
+    "FuelLevel": "",
+    "FuelLevelPct": "",
+    "FuelUsePerHour": "",
+    "Gear": "",
+    "Lap": "",
+    "LapBestLap": "",
+    "LapBestLapTime": "",
+    "LapCompleted": "",
+    "LapCurrentLapTime": "",
+    "LapDeltaToBestLap_DD": "",
+    "LapDeltaToBestLap": "",
+    "LapDeltaToOptimalLap_DD": "",
+    "LapDeltaToOptimalLap": "",
+    "LapDeltaToSessionBestLap_DD": "",
+    "LapDeltaToSessionBestLap": "",
+    "LapDeltaToSessionLastlLap_DD": "",
+    "LapDeltaToSessionLastlLap": "",
+    "LapDeltaToSessionOptimalLap_DD": "",
+    "LapDeltaToSessionOptimalLap": "",
+    "LapDist": "",
+    "LapDistPct": "",
+    "LapLasNLapSeq": "",
+    "LapLastLapTime": "",
+    "LapLastNLapTime": "",
+    "LFtempCL": "",
+    "LFtempCM": "",
+    "LFtempCR": "",
+    "LFwearL": "",
+    "LFwearM": "",
+    "LFwearR": "",
+    "LRtempCL": "",
+    "LRtempCM": "",
+    "LRtempCR": "",
+    "LRwearL": "",
+    "LRwearM": "",
+    "LRwearR": "",
+    "OilLevel": "",
+    "OilPress": "",
+    "OilTemp": "",
+    "PlayerCarClassPosition": "",
+    "PlayerCarDriverIncidentCount": "",
+    "PlayerCarIdx": "",
+    "PlayerCarMyIncidentCount": "",
+    "PlayerCarPosition": "",
+    "PlayerCarTeamIncidentCount": "",
+    "PlayerCarTowTime": "",
+    "PlayerTireCompound": "",
+    "PlayerTrackSurface": "",
+    "PlayerTrackSurfaceMaterial": "",
+    "RaceLaps": "",
+    "RelativeHumidity": "",
+    "RFtempCL": "",
+    "RFtempCM": "",
+    "RFtempCR": "",
+    "RFwearL": "",
+    "RFwearM": "",
+    "RFwearR": "",
+    "Roll": "",
+    "RollRate": "",
+    "RPM": "",
+    "RRtempCL": "",
+    "RRtempCM": "",
+    "RRtempCR": "",
+    "RRwearL": "",
+    "RRwearM": "",
+    "RRwearR": "",
+    "SessionFlags": "",
+    "SessionLapsRemain": "",
+    "SessionNum": "",
+    "SessionState": "",
+    "SessionTime": "",
+    "SessionTimeRemain": "",
+    "SessionUniqueID": "",
+    "ShiftGrindRPM": "",
+    "ShiftIndicatorPct": "",
+    "ShiftPowerPct": "",
+    "Skies": "",
+    "Speed": "",
+    "SteeringWheelAngle": "",
+    "SteeringWheelAngleMax": "",
+    "SteeringWheelPctDamper": "",
+    "SteeringWheelPctTorque": "",
+    "SteeringWheelPctTorqueSign": "",
+    "SteeringWheelPctTorqueSignStops": "",
+    "SteeringWheelPeakForceNm": "",
+    "SteeringWheelTorque": "",
+    "ThrottleRaw": "",
+    "TireSetsAvailable": "",
+    "TireSetsUsed": "",
+    "TrackTemp": "",
+    "TrackTempCrew": "",
+    "WeatherType": "",
+    "WindDir": "",
+    "WindVel": "",
 }
 # dictionary for bool data
 bool_dict = {
-  'IsInGarage': '',
-  'IsOnTrack': '',
-  'IsOnTrackCar': '',
-  'OnPitRoad': '',
-  'PitstopActive': '',
-  'PlayerCarInPitStall': '',
+    "IsInGarage": "",
+    "IsOnTrack": "",
+    "IsOnTrackCar": "",
+    "OnPitRoad": "",
+    "PitstopActive": "",
+    "PlayerCarInPitStall": "",
 }
 
 # this is our State class, with some helpful variables
@@ -151,9 +153,10 @@ class State:
     ir_connected = False
     last_car_setup_tick = -1
 
+
 # Class used to track lap completions
 class Counter:
-    def __init__(self, count = 1):
+    def __init__(self, count=1):
         self._count = count
 
     def get_count(self):
@@ -164,6 +167,7 @@ class Counter:
 
     count = property(get_count, set_count)
 
+
 # here we check if we are connected to iracing
 # so we can retrieve some data
 def check_iracing():
@@ -173,48 +177,61 @@ def check_iracing():
         state.last_car_setup_tick = -1
         # we are shutting down ir library (clearing all internal variables)
         ir.shutdown()
-        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + ' irsdk disconnected')
-    elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
+        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " irsdk disconnected")
+    elif (
+        not state.ir_connected
+        and ir.startup()
+        and ir.is_initialized
+        and ir.is_connected
+    ):
         state.ir_connected = True
-        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + ' irsdk connected')
+        print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " irsdk connected")
 
-#Get current lap
+
+# Get current lap
 def get_lap():
-    return ir['Lap']
+    return ir["Lap"]
 
-#Sends event to SIM when lap is completed
+
+# Sends event to SIM when lap is completed
 def send_lap_event(counter):
     current_lap = int(get_lap())
     i = counter.count
-    if(current_lap == i): 
+    if current_lap == i:
         counter.count = current_lap + 1
         print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " new lap")
-        ingest.send_event(event_type='Lap Complete',
-                            category='USER_DEFINED',
-                            dimensions={
-                                'driver': racer_name,
-                                'lap': str(metrics_dict['Lap']),
-                                'LapTime': str(metrics_dict['LapCurrentLapTime']),
-                                'LapDistance': str(metrics_dict['LapDist'])},
-                            properties={'session_id': str(metrics_dict['SessionUniqueID'])},
-                            timestamp=time.time() * 1000)
+        ingest.send_event(
+            event_type="Lap Complete",
+            category="USER_DEFINED",
+            dimensions={
+                "driver": args["name"],
+                "lap": str(metrics_dict["Lap"]),
+                "LapTime": str(metrics_dict["LapCurrentLapTime"]),
+                "LapDistance": str(metrics_dict["LapDist"]),
+            },
+            properties={"session_id": str(metrics_dict["SessionUniqueID"])},
+            timestamp=time.time() * 1000,
+        )
 
-#Sends metrics to SIM 
+
+# Sends metrics to SIM
 def send_metric(ir_json):
     for key, value in ir_json.items():
         ingest.send(
             gauges=[
-            {
-                'metric': 'iracing.' + key,
-                'value': value,
-                'timestamp': time.time() * 1000,
-                'dimensions': {
-                    'driver': racer_name, 
-                    'lap': str(ir_json['Lap']),
-                    'session_id': str(ir_json['SessionUniqueID'])
+                {
+                    "metric": "iracing." + key,
+                    "value": value,
+                    "timestamp": time.time() * 1000,
+                    "dimensions": {
+                        "driver": args["name"],
+                        "lap": str(ir_json["Lap"]),
+                        "session_id": str(ir_json["SessionUniqueID"]),
+                    },
                 }
-                
-            }])
+            ]
+        )
+
 
 def loop(json_dict):
     for key, value in json_dict.items():
@@ -224,19 +241,20 @@ def loop(json_dict):
     send_lap_event(lapCounter)
     send_metric(json_dict)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # initializing ir and state
     ir = irsdk.IRSDK()
     state = State()
     print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " iRacing started")
 
-    #instantiate lap counter for sending lap completion events
+    # instantiate lap counter for sending lap completion events
     lapCounter = Counter()
     ir.startup()
 
-    #Update lap counter if iRacing is already running
-    if (ir.is_connected):
-        lapCounter.count = ir['Lap'] + 1
+    # Update lap counter if iRacing is already running
+    if ir.is_connected:
+        lapCounter.count = ir["Lap"] + 1
 
     try:
         # infinite loop
@@ -249,7 +267,10 @@ if __name__ == '__main__':
             # sleep for 0.3 second
             # maximum you can use is 1/60
             # cause iracing updates data with 60 fps
-            print(datetime.now().strftime("%m/%d/%Y %H:%M:%S") + " successfully looped through the script.")
+            print(
+                datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+                + " successfully looped through the script."
+            )
             time.sleep(0.3)
     except KeyboardInterrupt:
         # press ctrl+c to exit
